2cecf871a6c1c69a062e6a132b3f071c7ee3383e
FRANCISCO JIMÉNEZ LOZANO <frajimloz@alu.edu.gva.es>
2023-11-20 11:45:43 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/BackGroundConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Superheroes
+{
+    public class BackGroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if ((bool)value)
+                    return Brushes.PaleGreen;
+                else
+                    return Brushes.IndianRed;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Superheroes/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Superheroes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int posicion = 0;
+
+        List<Superheroe> personaje;
+     
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            personaje = Superheroe.GetSamples();
+            xenvuevelDock.DataContext = personaje.FirstOrDefault();
+            
+
+        }
+
+        private void Pasar_Personaje_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) 
+        {
+            Image image = (Image)sender;
+
+            if (image.Name == "xDerechaImage" &&
+                posicion < 2)
+            {
+                posicion++;
+                xpaginaTextBlock.Text = posicion + 1 + "/3";
+                xenvuevelDock.DataContext = personaje[posicion];
+            }
+            else if (image.Name == "xIzquierdaImage" &&
+                posicion > 0) 
+            { 
+                posicion--;
+                xpaginaTextBlock.Text = posicion + 1 + "/3";
+                xenvuevelDock.DataContext = personaje[posicion];
+            }
+        }
+
+    }
+}
